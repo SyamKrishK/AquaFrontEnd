e28e12e40511cf036e7911ece26c8692f85fa512
SyamKrishK <syamkrishna28072001@gmail.com>
2023-07-07 15:11:50 +0530

    imp
    suparna7suppu admin ac

--- src/fireBase/firebase.js
+++ src/fireBase/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import 'firebase/auth';
 
 const app = firebase.initializeApp( {
-    apiKey: "AIzaSyCGyF_r-yjAGFfCQhdhoTGic4TyFmEkRMc",
-    authDomain: "aquarina-8527c.firebaseapp.com",
-    projectId: "aquarina-8527c",
-    storageBucket: "aquarina-8527c.appspot.com",
-    messagingSenderId: "318014606824",
-    appId: "1:318014606824:web:7919d3165ffc734652685d"
+    apiKey: "AIzaSyBgZ3l_m4vHqp9AOHCbJVWpzBNW07GisgI",
+    authDomain: "aquarina-001.firebaseapp.com",
+    projectId: "aquarina-001",
+    storageBucket: "aquarina-001.appspot.com",
+    messagingSenderId: "99816599565",
+    appId: "1:99816599565:web:bf025587a30465784ba544"
     // apiKey: "AIzaSyBsNAYte-6usdohgVPsKNoGtrWFx75mz_g",
     // authDomain: "expensetracker-6e5ba.firebaseapp.com",
     // projectId: "expensetracker-6e5ba",
@@ -15,6 +15,7 @@ const app = firebase.initializeApp( {
     // messagingSenderId: "259707873994",
     // appId: "1:259707873994:web:76133db07be3513da02233",
     // measurementId: "G-5Z284HHHEV"
+    
 });
 export const auth = app.auth();
 export default app; 
